fed02b1e07f0007ea6055c7a0296b9f957040f4f
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2025-09-18 13:38:16 -0400

    Finished Chapter 12 - Implemented Order/Invoice frontend

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 def signup(request):
@@ -44,3 +45,11 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect("home.index")
+
+
+@login_required
+def orders(request):
+    template_data = {}
+    template_data["title"] = "Orders"
+    template_data["orders"] = request.user.order_set.all()
+    return render(request, "accounts/orders.html", {"template_data": template_data})
