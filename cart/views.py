62137b59ab7478673c503efde7bae263d0eaa2b8
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2025-10-01 12:55:49 -0400

    3 Carts for Exam

--- cart/views.py
+++ cart/views.py
@@ -6,25 +6,28 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    cart_total = 0
-    movies_in_cart = []
-    cart = request.session.get("cart", {})
-    movie_ids = list(cart.keys())
-    if movie_ids != []:
-        movies_in_cart = Movie.objects.filter(id__in=movie_ids)
-        cart_total = calculate_cart_total(cart, movies_in_cart)
     template_data = {}
+    for i in range(1, 4):
+        cart_total = 0
+        movies_in_cart = []
+        cart = request.session.get("cart" + str(i), {})
+        movie_ids = list(cart.keys())
+        if movie_ids != []:
+            movies_in_cart = Movie.objects.filter(id__in=movie_ids)
+            cart_total = calculate_cart_total(cart, movies_in_cart)
+        template_data["movies_in_cart" + str(i)] = movies_in_cart
+        template_data["cart" + str(i) + "_total"] = cart_total
+
     template_data["title"] = "Cart"
-    template_data["movies_in_cart"] = movies_in_cart
-    template_data["cart_total"] = cart_total
     return render(request, "cart/index.html", {"template_data": template_data})
 
 
 def add(request, id):
     get_object_or_404(Movie, id=id)
-    cart = request.session.get("cart", {})
+    cart_num = request.POST["cartnum"]
+    cart = request.session.get("cart" + str(cart_num), {})
     cart[id] = request.POST["quantity"]
-    request.session["cart"] = cart
+    request.session["cart" + str(cart_num)] = cart
     return redirect("cart.index")
 
 
